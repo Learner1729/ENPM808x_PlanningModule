b290b3bad5c8c5e75bae61f9735723c4c54b1ab3
Learner1729 <ashishpatel0894@gmail.com>
2018-11-30 23:56:11 -0500

    Created MOCK class and added it in test.cpp file Modified README.md file for better visual appeal

--- README.md
+++ README.md
@@ -14,28 +14,6 @@
 
 ## <a name="license"></a> License
 This project is under the [MIT License](./LICENSE)
-```
-Copyright (c) 2018 Ashish Patel
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-SOFTWARE.
-
-```
 
 ## <a name="overview"></a> Overview 
 This repo is a simple global path planning module, developed for ACME Robotics. Given an initial and final location, the module can compute shortest path using A* algorithm to reach the final location. The purpose of this project is to design a global path planning module that can be integrated to build a robotic system to navigate autonomously in a known environment.
@@ -63,7 +41,7 @@ Assumptions:
 ## <a name="class"></a> Class Diagram for GMocking
 <p align="center">
 <a target="_blank"><img src="UML/initial/GMOCK.png"
-alt="WorkUnderProgress" width="480" height="480" border="10" />
+alt="WorkUnderProgress" width="640" height="480" border="10" />
 </a>
 </p>
 
--- test/test.cpp
+++ test/test.cpp
@@ -1,5 +1,72 @@
-#include <gtest/gtest.h>
+/**
+ * MIT License
+ *
+ * Copyright (c) 2018 Ashish Patel
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to 
+ * deal in the Software without restriction, including without limitation the 
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in 
+ * all copies or substantial portions of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+ * IN THE SOFTWARE
+ */
 
-TEST(dummy, should_pass) {
-  EXPECT_EQ(1, 1);
+/**
+ * @file test.cpp
+ * @version 1.0
+ * @author Ashish Patel
+ * @brief The file contains GMocking implementation
+ * @last-modified 11-30-2018
+ */
+
+// Including externally dependent library file used to create
+// window for visualizing map of an environment
+#include <SDL.h>
+
+// Including google unit testing header files
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+
+// User-defined header file
+#include "VisualizeMapSDL.h"
+#include "AStar.h"
+
+// Using mock in the testing
+using ::testing::AtLeast;
+using ::testing::Return;
+using ::testing::Ge;
+
+// developing mock class 
+class MockVisualMapSDL : public VisualizeMapSDL {
+public:
+  MOCK_METHOD0(init, bool());
+  MOCK_CONST_METHOD0(isClosed, bool());
+  MOCK_CONST_METHOD0(clear, void());
+  MOCK_METHOD0(detectEvent, void());
+  MOCK_CONST_METHOD0(callDelay, void());
+  MOCK_METHOD3(drawPixel, void(int, int, int));
+  MOCK_CONST_METHOD0(updateWindow, void());
+  MOCK_METHOD1(setDelay, void(int));
+};
+
+TEST(Test, CanDrawSomething) {
+  MockVisualMapSDL *visual = nullptr;
+  AStar path(visual);
+  //pathFinding.generateMap();
+  //MockVisualMapSDL mocksdl;
+
+  //while(!pathFinding.inloop()) {
+  //}
+  //return 0;
 }
